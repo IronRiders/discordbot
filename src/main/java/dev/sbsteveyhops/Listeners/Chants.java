dd9274024caaae45a0a04e67d659f84d0b6ea201
SBsteveyHops <salvobonsma@gmail.com>
2023-04-04 21:02:15 -0700

    Fixed Chants

--- src/main/java/dev/sbsteveyhops/Listeners/Chants.java
+++ src/main/java/dev/sbsteveyhops/Listeners/Chants.java
@@ -8,42 +8,18 @@ public class Chants extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) {
-            return;
-        }
+        if (event.getAuthor().isBot()) { return; }
 
         String message = event.getMessage().getContentRaw().toLowerCase();
-        if (message.contains("who rides")) {
-            send("We ride!", event);
-            return;
-        }
-        if (message.contains("who are we")) {
-            send("Iron Riders!", event);
-            return;
-        }
-        if (message.contains("how hungry")) {
-            send("Famished!", event);
-            return;
-        }
-        if (message.contains("for what")) {
-            send("Victory!", event);
-            return;
-        }
-        if (message.equalsIgnoreCase("iron")) {
-            send("Riders!", event);
-            return;
-        }
-        if (message.contains("go riders")) {
-            send(claps, event);
-            return;
-        }
-        if (message.equals("41")) {
-            send("80", event);
-            return;
-        }
+        if (message.contains("who rides")) { send("We ride!", event); return; }
+        if (message.contains("who are we")) { send("Iron Riders!", event); return; }
+        if (message.contains("how hungry")) { send("Famished!", event); return; }
+        if (message.contains("for what")) { send("Victory!", event); return; }
+        if (message.equalsIgnoreCase("iron")) { send("Riders!", event); return; }
+        if (message.contains("go riders")) { send(claps, event); return; }
+        if (message.equals("41")) { send("80", event); return; }
         if (message.equalsIgnoreCase("red alliance") || message.equalsIgnoreCase("blue alliance")) {
-            send(claps, event);
-        }
+            send(claps, event); }
     }
     private void send(String message, MessageReceivedEvent event) {
         event.getChannel().sendMessage(message).queue();
