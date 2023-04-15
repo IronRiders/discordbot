92a304440ad0f71d2f0f5f854abefd2df08cf25b
SBsteveyHops <salvobonsma@gmail.com>
2023-04-15 16:50:39 -0700

    tiny fixe

--- src/main/java/org/ironriders/discordbot/Listeners/Chants.java
+++ src/main/java/org/ironriders/discordbot/Listeners/Chants.java
@@ -18,7 +18,8 @@ public class Chants extends ListenerAdapter {
         if (Pattern.matches("(?i)how hungry[?]*?", message)) { send("Famished!", event); return; }
         if (Pattern.matches("(?i)for what[?]*?", message)) { send("Victory!", event); return; }
         if (Pattern.matches("(?i)iron", message)) { send("Riders!", event); return; }
-        if (Pattern.matches("(?i)(red|blue) alliance[!,.]*?|(?i)go riders[!,.]*?", message)) { send(claps, event); return; }
+        if (Pattern.matches("(?i)(red|blue) alliance[!,.]*?|(?i)go riders[!,.]*?", message)) {
+            send(claps, event); return; }
         if (Pattern.matches("41", message)) { send("80", event); }
     }
     private void send(String message, MessageReceivedEvent event) {
