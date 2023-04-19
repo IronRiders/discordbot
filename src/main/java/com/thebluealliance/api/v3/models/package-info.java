a69b9a228dbaba261032e85f220c5dadded668c6
SBsteveyHops <salvobonsma@gmail.com>
2023-04-19 16:50:35 -0700

    maybe better

--- src/main/java/com/thebluealliance/api/v3/models/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains the models represented by JSON data from The Blue Alliance API.
- *
- * @see com.thebluealliance.api.v3.Deserializer
- */
-package com.thebluealliance.api.v3.models;
--- src/main/java/com/thebluealliance/api/v3/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides the classes necessary to access The Blue Alliance API and deserialize JSON data obtained from the API into object models
- */
-package com.thebluealliance.api.v3;
