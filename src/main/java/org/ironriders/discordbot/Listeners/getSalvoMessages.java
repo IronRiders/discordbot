f0cda77a5c66ca2b8ca65500ce46d7821e761b55
Saturn corgi <tylerclayton183@gmail.com>
2024-05-15 13:12:11 -0700

    added !saveDataset

--- src/main/java/org/ironriders/discordbot/Listeners/getSalvoMessages.java
+++ src/main/java/org/ironriders/discordbot/Listeners/getSalvoMessages.java
@@ -1,5 +1,10 @@
 package org.ironriders.discordbot.Listeners;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -7,22 +12,29 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.restaction.pagination.MessagePaginationAction;
 
-public class getSalvoMessages extends ListenerAdapter {
+@SuppressWarnings("unused")
+class getSavloMessages extends ListenerAdapter {
     private static final String TARGET_USER_ID = "775852592093593600";
+    private List<String> messages = new ArrayList<>();
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("!fetchMessages")) {
+        String messageContent = event.getMessage().getContentRaw();
+        if (messageContent.equalsIgnoreCase("!fetchMessages")) {
             fetchAllMessages(event.getGuild());
             event.getChannel().sendMessage("Started fetching messages from user ID: " + TARGET_USER_ID).queue();
+        } else if (messageContent.equalsIgnoreCase("!saveDataset")) {
+            saveMessagesToFile("dataset.txt");
+            event.getChannel().sendMessage("Messages have been saved to dataset.txt").queue();
         }
     }
 
     public void fetchAllMessages(Guild guild) {
         for (TextChannel channel : guild.getTextChannels()) {
-            MessagePaginationAction messages = channel.getIterableHistory();
-            messages.forEachAsync(message -> {
+            MessagePaginationAction messagesAction = channel.getIterableHistory();
+            messagesAction.forEachAsync(message -> {
                 if (message.getAuthor().getId().equals(TARGET_USER_ID)) {
+                    messages.add(message.getContentDisplay());
                     System.out.println("Found message: " + message.getContentDisplay());
                 }
                 return true; // Continue fetching
@@ -30,6 +42,16 @@ public class getSalvoMessages extends ListenerAdapter {
                 throwable.printStackTrace();
                 return null;
             });
+        }
+    }
+
+    public void saveMessagesToFile(String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (String message : messages) {
+                writer.write(message + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
