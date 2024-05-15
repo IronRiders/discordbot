c16f50c8a15e83dc280ea8d3e307c4b148757841
Saturn corgi <tylerclayton183@gmail.com>
2024-05-15 13:21:02 -0700

    Bot now sends dataset after saving

--- src/main/java/org/ironriders/discordbot/Listeners/getSalvoMessages.java
+++ src/main/java/org/ironriders/discordbot/Listeners/getSalvoMessages.java
@@ -1,16 +1,20 @@
 package org.ironriders.discordbot.Listeners;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.restaction.pagination.MessagePaginationAction;
+import net.dv8tion.jda.api.utils.FileUpload;
 
 @SuppressWarnings("unused")
 class getSavloMessages extends ListenerAdapter {
@@ -26,6 +30,7 @@ class getSavloMessages extends ListenerAdapter {
         } else if (messageContent.equalsIgnoreCase("!saveDataset")) {
             saveMessagesToFile("dataset.txt");
             event.getChannel().sendMessage("Messages have been saved to dataset.txt").queue();
+            sendDatasetAsAttachment(event.getChannel(), "dataset.txt");
         }
     }
 
@@ -52,6 +57,20 @@ class getSavloMessages extends ListenerAdapter {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void sendDatasetAsAttachment(MessageChannel channel, String fileName) {
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
+                channel.sendFiles((Collection<? extends FileUpload>) file).queue();
+            } catch (Exception e) {
+                channel.sendMessage("Failed to send the dataset file. Unknown error").queue();
+            }
+           
+        } else {
+            channel.sendMessage("Failed to send the dataset file. Unable to find dataset").queue();
         }
     }
 }
