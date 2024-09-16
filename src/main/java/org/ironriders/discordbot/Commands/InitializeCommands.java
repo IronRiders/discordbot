f1133c79075df00af5064b131039515d473be7ee
Saturn corgi <tylerclayton183@gmail.com>
2024-09-15 19:40:34 -0700

    made it ask for permission.VIEW_CHANNEL

--- src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
+++ src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.Permission;
 
 public class InitializeCommands extends ListenerAdapter {
     @Override
@@ -12,21 +13,21 @@ public class InitializeCommands extends ListenerAdapter {
         event.getJDA().updateCommands().addCommands(
                 Commands.slash("tba", "Retrieves some information on any team of your choosing.")
                         .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false)
-                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                        .enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("thebluealliance", "Retrieves some information on any team of your " +
                                 "choosing.")
                         .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false)
-                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                        .enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("teaminfo", "Retrieves some information on our team.")
-                .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                .enabledFor(Permission.VIEW_CHANNEL),
 
                 // Instructional Commands
-                Commands.slash("help", "Retrieves a key of all the commands plus how to contribute.").setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                Commands.slash("help", "Retrieves a key of all the commands plus how to contribute.").enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("resources", "Retrieves an embed with our teams resources.")
-                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                        .enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("roster", "Provides instructions on complete ones membership.")
                         .addOption(OptionType.MENTIONABLE, "notify", "Member to notify", false)
-                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                        .enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("schedule", "Provides a schedule for the selected department.")
                         .addOptions(
                                 new OptionData(
@@ -40,9 +41,9 @@ public class InitializeCommands extends ListenerAdapter {
                                         .addChoice("Design/CAD Department", "cad")
                                         .addChoice("Drive Team", "drive")
                         )
-                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                        .enabledFor(Permission.VIEW_CHANNEL),
                 Commands.slash("chants", "Retrieves a key of all our chants.")
-                .setDefaultPermissions(DefaultMemberPermissions.ENABLED)
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor)
         ).queue();
     }
 }
