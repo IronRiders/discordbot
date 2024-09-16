e994ee6a8172091a3a584f42ea61e69197cd6a74
Saturn corgi <tylerclayton183@gmail.com>
2024-09-15 19:55:19 -0700

    sorry i done with writing these

--- src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
+++ src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.*;
+import net.dv8tion.jda.apt.*;
 
 public class InitializeCommands extends ListenerAdapter {
     @Override
