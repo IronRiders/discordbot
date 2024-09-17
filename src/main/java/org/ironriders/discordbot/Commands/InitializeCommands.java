841fb8f74ba7aac09250a8688205a7f2f2b05344
Tyler <104324278+Saturncorgi@users.noreply.github.com>
2024-09-17 14:39:47 -0700

    Added default permissions. fix maybe? (#23)

--- src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
+++ src/main/java/org/ironriders/discordbot/Commands/InitializeCommands.java
@@ -2,6 +2,7 @@ package org.ironriders.discordbot.Commands;
 
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -11,17 +12,22 @@ public class InitializeCommands extends ListenerAdapter {
     public void onReady(ReadyEvent event) {
         event.getJDA().updateCommands().addCommands(
                 Commands.slash("tba", "Retrieves some information on any team of your choosing.")
-                        .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false),
+                        .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false)
+                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
                 Commands.slash("thebluealliance", "Retrieves some information on any team of your " +
                                 "choosing.")
-                        .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false),
-                Commands.slash("teaminfo", "Retrieves some information on our team."),
+                        .addOption(OptionType.INTEGER, "teamnumber", "Team Number", false)
+                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                Commands.slash("teaminfo", "Retrieves some information on our team.")
+                .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
 
                 // Instructional Commands
-                Commands.slash("help", "Retrieves a key of all the commands plus how to contribute."),
-                Commands.slash("resources", "Retrieves an embed with our teams resources."),
+                Commands.slash("help", "Retrieves a key of all the commands plus how to contribute.").setDefaultPermissions(DefaultMemberPermissions.ENABLED),
+                Commands.slash("resources", "Retrieves an embed with our teams resources.")
+                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
                 Commands.slash("roster", "Provides instructions on complete ones membership.")
-                        .addOption(OptionType.MENTIONABLE, "notify", "Member to notify", false),
+                        .addOption(OptionType.MENTIONABLE, "notify", "Member to notify", false)
+                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
                 Commands.slash("schedule", "Provides a schedule for the selected department.")
                         .addOptions(
                                 new OptionData(
@@ -34,8 +40,10 @@ public class InitializeCommands extends ListenerAdapter {
                                         .addChoice("Build Department", "build")
                                         .addChoice("Design/CAD Department", "cad")
                                         .addChoice("Drive Team", "drive")
-                        ),
+                        )
+                        .setDefaultPermissions(DefaultMemberPermissions.ENABLED),
                 Commands.slash("chants", "Retrieves a key of all our chants.")
+                .setDefaultPermissions(DefaultMemberPermissions.ENABLED)
         ).queue();
     }
 }
